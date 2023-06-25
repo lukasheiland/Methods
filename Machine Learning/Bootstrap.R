76da15a0d5fa8f824065533dc310bb0a809dbca1
Lukas <code@heiland.earh>
2023-06-25 19:00:12 +0200

    Update Bootstrap.R

--- Machine Learning/Bootstrap.R
+++ Machine Learning/Bootstrap.R
@@ -4,8 +4,10 @@
 
 ## For calculation of CONFIDENCE INTERVALS
 
-# Resample fractions of the data with replacement and fit models.
-# The spread of the estimatesm will correspond to the estimate distribution/uncertainty.
+# Resample fractions of the data __with replacement__ and fit models.
+# This is based on the idea that the process of resampling from the data set on hand represents the sampling process that got the data set from the original population in the first place.
+# How many bootstrap samples to get a good Monte Carlo approximation to the bootstrap result? Increase the size until you get convergence. No one number fits all problems.
+# The spread of the estimates will correspond to the estimate distribution/uncertainty.
 # There is a proof, that this holds, regardless of any distributional theory.
 
 # Use this for CIs of longer pipelines with multiple methods (e.g., including AIC selection), to get correct CI (and p-values)!
